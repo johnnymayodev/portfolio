9fc9a4fb1472e069ea7b397168074e76f84a878e
Johnny <jmnaze123@gmail.com>
2023-11-24 23:12:16 -0500

    Create app.js

---
+++ static/js/app.js
@@ -0,0 +1,15 @@
+console.log("app.js loaded");
+
+var year = new Date().getFullYear();
+var month = new Date().getMonth() + 1;
+var day = new Date().getDate();
+
+
+footer = document.getElementById("footer");
+footer.getElementsByTagName("p")[0].innerHTML += year;
+
+
+container = document.getElementById("container");
+setTimeout(function() {
+    container.style.opacity = 1;
+}, 1000);
