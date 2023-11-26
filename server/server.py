df704b272166b1dc4a08d6f52c7dfd03c6ce4e98
Johnny <jmnaze123@gmail.com>
2023-11-26 17:53:29 -0500

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../templates', static_folder='../static')
+app = Flask(__name__, template_folder="../templates", static_folder="../static")
 port = 55055
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True, port=port)
