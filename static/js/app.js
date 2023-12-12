5cfd4066aef1e1418ab6ae28fa1682eb5a912983
Johnny <jmnaze123@gmail.com>
2023-12-12 10:44:18 -0500

    Update app.js
    removed fade in

--- static/js/app.js
+++ static/js/app.js
@@ -4,12 +4,5 @@ var year = new Date().getFullYear();
 var month = new Date().getMonth() + 1;
 var day = new Date().getDate();
 
-
 footer = document.getElementById("footer");
 footer.getElementsByTagName("p")[0].innerHTML += year;
-
-
-container = document.getElementById("container");
-setTimeout(function() {
-    container.style.opacity = 1;
-}, 1000);
