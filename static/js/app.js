8d4f434e97313a6d552d0db35dfb259a3c2c955c
Johnny <jmnaze123@gmail.com>
2024-04-27 22:33:21 -0400

    Scroll helper now works in dark mode

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,26 @@
-console.log("You're still a nerd.");
-console.log("app.js loaded");
+const log = console.log;
+
+log("Hello!");
+log("app.js loaded");
+
+const scrollHelperElem = document.querySelector(".scroll-helper");
+
+updateScrollHelper();
+
+setInterval(() => {
+  updateScrollHelper();
+}, 1000);
+
+function updateScrollHelper() {
+  // get user prefer dark mode or light mode
+  let prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+  // check if user prefer dark mode
+  if (prefersDarkScheme.matches) {
+    scrollHelperElem.src =
+      "https://img.icons8.com/android/100/f6f6f6/expand-arrow.png";
+  } else {
+    scrollHelperElem.src =
+      "https://img.icons8.com/android/100/323232/expand-arrow.png";
+  }
+}
