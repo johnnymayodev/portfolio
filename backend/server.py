36bf24a3edb35bf5edfc448a3e8383734fb17237
Johnny <jmnaze123@gmail.com>
2025-02-13 23:13:19 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -10,15 +10,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<path>")
-def page(path):
-    if path.endswith(".html"):
-            path = path[:-5]  # Remove the last 5 characters (.html)
-
-    if not os.path.exists(f"templates/{path}.html"):
-        return render_template("404.html"), 404
-
-    return render_template(f"{path}.html")
+@app.route("/wip")
+def wip():
+    return render_template("wip.html")
 
 
 if __name__ == "__main__":
