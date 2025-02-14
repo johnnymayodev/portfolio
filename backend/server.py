54cf3090bcfacca28cf17e68fae9115665cec439
Johnny <jmnaze123@gmail.com>
2025-02-13 23:20:21 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template #type: ignore
-import os
+
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 port = 3000
@@ -13,6 +13,11 @@ def index():
 @app.route("/wip")
 def wip():
     return render_template("wip.html")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html")
 
 
 if __name__ == "__main__":
